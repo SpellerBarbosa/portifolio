ee5518e221cc5304e62b4231f08c32f27db726e1
Fernando Barbosa <bemar.host@gmail.com>
2025-04-24 07:12:27 -0300

    Constução portifolio

---
+++ components/Menu.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class=" flex justify-between text-[#F4F4F5] w-[90%] border-2">
+        <button class="w-[50px] h-[50px] flex flex-col justify-evenly items-center">
+            <span class="bg-[#f4f4f5] w-[90%] h-[2px] block"></span>
+            <span class="bg-[#f4f4f5] w-[90%] h-[2px] block"></span>
+            <span class="bg-[#f4f4f5] w-[90%] h-[2px] block"></span>
+        </button>
+        <h1>Fernando Barbosa</h1>
+        <nav>
+            <ul class="hidden">
+                <li>Inicio</li>
+                <li>Sobre</li>
+                <li>Skills</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="bg-[#0E1116] w-screen h-screen">
+        <Menu />
+    </main>
+</template>
